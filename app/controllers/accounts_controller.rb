2e96140eb8531e48a06d7ef7ca61bdd1ffdcbc85
PaulaEs <paulaproxy@gmail.com>
2013-08-14 02:18:34 +0200

    Adding methodes to controller. Views to accounts.

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,15 +1,31 @@
 class AccountsController < ApplicationController
+	before_filter :find_client
+	before_filter :find_account, only: [:show, :destroy]
 
 	def index
+		@account = @client.accounts.order("id")
 	end
 
 	def show
 	end
 
 	def create
+		@client.accounts.create!
+		redirect_to client_accounts_path(@client)
 	end
 
 	def destroy
+		@account.destroy
+		redirect_to client_accounts_path(@client)
 	end
-	
+
+	private
+ 		def find_client
+    	@client = Client.find(params[:client_id])
+  	end
+
+  	def find_account
+  		@account = @client.accounts.find(params[:id])
+  	end
+
 end

7421d4b241fe30ef08b26311b09f5ac2bd2e0e89
PaulaEs <paulaproxy@gmail.com>
2013-08-21 23:50:19 +0200

    Minor layout changes.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -26,7 +26,7 @@ class ClientsController < ApplicationController
 
     if @client.save
       ClientMailer.welcome(@client).deliver
-      redirect_to login_path, notice: 'Welcome in MiniBank! Please login.'
+      redirect_to login_path, notice: 'Welcome to MiniBank! Please login.'
     else
       render action: 'new' 
     end
